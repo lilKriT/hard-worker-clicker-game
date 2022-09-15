724cd6897eaa4173b733f66305d83064b8205b18
Piotr Filipiak <kiddhkane@gmail.com>
2022-09-15 22:20:46 +0200

    Clicking works.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-const { default: Main } = require("./components/Main");
+// const { default: Main } = require("./components/Main");  // what is this?
+import { useState } from "react";
+import Main from "./components/Main";
 
 function App() {
-  return <Main />;
+  const [clicks, setClicks] = useState(0);
+
+  const click = () => {
+    console.log("Player clicked");
+    setClicks(clicks + 1);
+  };
+
+  return <Main clicks={clicks} click={click} />;
 }
 
 export default App;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-const Main = () => {
+const Main = ({ clicks, click }) => {
   return (
     <div className="main">
       <h2>Hard workers</h2>
-      <button className="clickHere">Click here</button>
+      <button className="clickHere" onClick={click}>
+        Click here
+      </button>
       <h3>
-        Clicks: <span className="amountOfClicks"></span>
+        Clicks: <span className="amountOfClicks">{clicks}</span>
       </h3>
     </div>
   );
