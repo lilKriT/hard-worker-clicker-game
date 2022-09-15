01d9f93306080692cdac99efe1948acbc76cf800
Piotr Filipiak <kiddhkane@gmail.com>
2022-09-15 22:29:07 +0200

    Started autosave.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 // const { default: Main } = require("./components/Main");  // what is this?
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Main from "./components/Main";
 
 function App() {
   const [clicks, setClicks] = useState(0);
+  const [playerState, setPlayerState] = useState({});
 
   const click = () => {
     console.log("Player clicked");
     setClicks(clicks + 1);
   };
+
+  useEffect(() => {
+    console.log("Starting autosave");
+    setInterval(() => {
+      console.log("Autosave");
+    }, 3 * 1000);
+
+    localStorage.setItem("Player State", {});
+  }, []);
 
   return <Main clicks={clicks} click={click} />;
 }
