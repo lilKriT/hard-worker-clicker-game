66e5550ec1ce39b6358952bc4a230db04e28403a
Piotr Filipiak <kiddhkane@gmail.com>
2022-11-05 21:56:21 +0100

    Revaming some stuff.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
-// const { default: Main } = require("./components/Main");  // what is this?
-import { useState, useEffect } from "react";
-import Main from "./components/Main";
+import Game from "./components/Game";
 
 function App() {
-  const [clicks, setClicks] = useState(0);
-  const [cash, setCash] = useState(0);
-  const [playerState, setPlayerState] = useState({});
-  const [playerStats, setPlayersStats] = useState({});
-
-  const click = () => {
-    console.log("Player clicked");
-    setClicks(clicks + 1);
-  };
-
-  useEffect(() => {
-    console.log("Starting autosave");
-    setInterval(() => {
-      console.log("Autosave");
-    }, 3 * 1000);
-
-    localStorage.setItem("Player State", {});
-  }, []);
-
-  return <Main clicks={clicks} click={click} cash={cash} />;
+  return <Game />;
 }
 
 export default App;
---
+++ src/components/Game.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+
+const Game = () => {
+  const tick = () => {
+    console.log("tick");
+  };
+
+  return <div className="game bg-slate-400 min-h-screen">Game</div>;
+};
+
+export default Game;
